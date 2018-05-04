a8903d574abe804b316cf49812cc9e40343719bd
Fabiano M. Dutra <fmbdutra@gmail.com>
2018-05-04 15:59:45 -0300

    Alteração baseado no Adriano

--- exemplo02/src/test/java/pucrs/s2b20181/exemplo02/AppTest.java
+++ exemplo02/src/test/java/pucrs/s2b20181/exemplo02/AppTest.java
@@ -4,43 +4,38 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collection;
- 
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-import org.junit.runner.RunWith;
 
 @RunWith(Parameterized.class)
-public class AppTest 
-{
-	
-	private int input1;
-	private int input2;
-	private int expectedR;
+public class AppTest {
 
-	public void AppCheckTest(int inputNum1, int inputNum2, int expectedResult) {
-		this.input1 = inputNum1;
-		this.input2 = inputNum2;
-		this.expectedR = expectedResult;		
-	}
+   private int n1;
+   private int n2;
+   private int resultado;
 	
+     public AppTest(int n1, int n2, int resultado) {
+        this.n1 = n1;
+        this.n2 = n2;
+        this.resultado = resultado;
+     }
 	
-	@Test
-    public void testApp()
-    {
-	   assertEquals(expectedR, App.somar(input1, input2));		
-	}
-	
-	
-	@Parameters(name = "{index}: {0}|{1}|{2}|{3}")
-	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][]{
-			{2, 2, 4}, //1
-			{5, 8, 13}, //2
-			{1, 1, 2}, //3
-			{20, 30, 50} //4
-			
-		});
-	}
-}
+     @Test
+     public void testeParametrizacaoJUnit() {
+    	 assertEquals(resultado, App.somar(n1, n2));
+     }
 
+
+     @Parameters(name = "{index}: {0}|{1}|{2}|{3}")
+     public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+           {0, 0, 0},
+           {0, 1, 1},
+           {1, 1, 2},
+           {1, 2, 3}
+        });
+     }
+}     
