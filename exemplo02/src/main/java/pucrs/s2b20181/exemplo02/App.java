5f06514bc83a7b334b123b068f30e987b7f83341
Fabiano M. Dutra <fmbdutra@gmail.com>
2018-05-04 15:56:43 -0300

    Alteração 1

--- exemplo02/src/main/java/pucrs/s2b20181/exemplo02/App.java
+++ exemplo02/src/main/java/pucrs/s2b20181/exemplo02/App.java
@@ -1,13 +1,23 @@
 package pucrs.s2b20181.exemplo02;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+			
+			int a = 20;
+			int b = 30;
+			
+			somar(a,b);
+			
+			System.out.println(a+b);
+			
+			
+				
+		}
+		
+		
+		public static int somar(int a, int b) {
+			int r = a + b;
+			return r;
+		}
 }
--- exemplo02/src/test/java/pucrs/s2b20181/exemplo02/AppTest.java
+++ exemplo02/src/test/java/pucrs/s2b20181/exemplo02/AppTest.java
@@ -1,20 +1,46 @@
 package pucrs.s2b20181.exemplo02;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+ 
 import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
 
-/**
- * Unit test for simple App.
- */
+@RunWith(Parameterized.class)
 public class AppTest 
 {
-    /**
-     * Rigourous Test :-)
-     */
+	
+	private int input1;
+	private int input2;
+	private int expectedR;
+
+	public void AppCheckTest(int inputNum1, int inputNum2, int expectedResult) {
+		this.input1 = inputNum1;
+		this.input2 = inputNum2;
+		this.expectedR = expectedResult;		
+	}
+	
+	
 	@Test
     public void testApp()
     {
-        assertTrue( true );
-    }
+	   assertEquals(expectedR, App.somar(input1, input2));		
+	}
+	
+	
+	@Parameters(name = "{index}: {0}|{1}|{2}|{3}")
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][]{
+			{2, 2, 4}, //1
+			{5, 8, 13}, //2
+			{1, 1, 2}, //3
+			{20, 30, 50} //4
+			
+		});
+	}
 }
+
