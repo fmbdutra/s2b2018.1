d7592dce594d901f1ea86ae206ba7402219803ca
Fabiano M. Dutra <fmbdutra@gmail.com>
2018-05-04 16:11:50 -0300

    Remove 'void' from constructor AppTest

--- exemplo02/src/test/java/pucrs/s2b20181/exemplo02/AppTest.java
+++ exemplo02/src/test/java/pucrs/s2b20181/exemplo02/AppTest.java
@@ -6,36 +6,37 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
 
 @RunWith(Parameterized.class)
-public class AppTest {
+public class AppTest
+{
 
-   private int n1;
-   private int n2;
-   private int resultado;
+   private int input1;
+   private int input2;
+   private int expectedR;
 	
-     public AppTest(int n1, int n2, int resultado) {
-        this.n1 = n1;
-        this.n2 = n2;
-        this.resultado = resultado;
+     public AppTest(int inputNum1, int inputNum2, int expectedResult) {
+        this.input1 = inputNum1;
+        this.input2 = inputNum2;
+        this.expectedR = expectedResult;
      }
 	
      @Test
-     public void testeParametrizacaoJUnit() {
-    	 assertEquals(resultado, App.somar(n1, n2));
+     public void testApp() {
+    	 assertEquals(expectedR, App.somar(input1, input2));
      }
 
 
      @Parameters(name = "{index}: {0}|{1}|{2}|{3}")
      public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-           {0, 0, 0},
-           {0, 1, 1},
-           {1, 1, 2},
-           {1, 2, 3}
+           {40, 10, 50}, //1
+           {20, 40, 60}, //2
+           {15, 15, 30}, //3
+           {4, 6, 10} //4
         });
      }
 }     
